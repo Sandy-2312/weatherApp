c52755b4b549460f4010092bb902e2b44f4a1bc6
Sandy-2312 <sandeepkumar1162b@gmail.com>
2025-06-03 22:09:23 +0530

    ReadMe file

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # weatherApp
-Creating a weather web application
+#### SkyNote - A weather application
+
+This is a weather app that retreives and displays real-time weather data from the OpenWeatherMap API and displays attributes like weather condition, location, temperature and more
+Relevant icons are displayed, displaying the current weater condition based on searched location and day
+
+
+## Instructions
+
+Follow these steps to run this project on your machine:
+
+1. Clone the repository:-
+
+    ```bash
+    git clone https://github.com/Sandy-2312/weatherApp
+    ```
+
+Folder Structure:- 
+
+```
+weatherApp/src
+   src/index.html
+   src/output.css
+   src/file.js
+```
+
+
+2. Add Your API Key:-
+
+    Replace "YOUR_API_KEY" in the script.js file with your actual Open Weather Map Api key.
+
+    ```bash
+    const API_KEY = "YOUR_API_KEY";
+    ```
+
+    You can get a free API key from [Open Weather Map](https://openweathermap.org/).
+
+
+3. Open in Browser:-
+
+    Open index.html on your browser using a local port to view the SkyNote web application 
+
+
+
+## What does it Do?
+
+* Allows users to search for weather forecasts by city name
+* Allow users to search weather forecasts for current location
+* Displays recently searched cities in a dropdown
+* Displays weather details including location, temperature, wind and more displayed via icons showing the current weather conditions
+* Simple and responsive
+* Real-time error handling for best user experience
+
+
+
+## Contact Me 
+
+#### Developed by Sandeep Kumar Dalai (with help from Open Weather Map API!)
+
+#### Project link:- [SkyNote - Weather Application](https://github.com/Sandy-2312/weatherApp)
+
+#### Thankyou for Reading!
--- src/file.js
+++ src/file.js
@@ -1,97 +1,103 @@
-//Navbar Dropdown on Clicking Profile Image
+//Getting IDs from elements for menu dropdown
 const button = document.getElementById("user-menu-button");
 const menu = document.getElementById("user-dropdown");
 
-  //Display menu on click
-  button.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-  });
+//Navbar Dropdown on Clicking Profile Image
+button.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
 
-  //Hide menu when clicking outside
-  window.addEventListener("click", (e) => {
-    if (!button.contains(e.target) && !menu.contains(e.target)) {
-      menu.classList.add("hidden");
-    }
-  });
+//Hide menu when clicking outside
+window.addEventListener("click", (e) => {
+  if (!button.contains(e.target) && !menu.contains(e.target)) {
+    menu.classList.add("hidden");
+  }
+});
 
  
 
-  
-const API_KEY = 'API_Key'; // Replaced with my OpenWeatherMap API key
+//API Key to connect to API provider 
+const API_KEY = '1a487a0699ca0d5a5a4172db066380f1'; // Replaced with my OpenWeatherMap API key
 
-const searchBtn = document.getElementById('searchBtn');
-const locBtn = document.getElementById('locBtn');
-const recentCitiesEl = document.getElementById('recentCities');
-const cityInput = document.getElementById('cityInput');
-const errorEl = document.getElementById('error');
-const currentWeatherEl = document.getElementById('currentWeather');
-const forecastEl = document.getElementById('forecast');
-const fiveDays = document.getElementById('fiveDay')
+//Getting IDs from elements to display Weather
+const searchCityBtn = document.getElementById('searchCityBtn');
+const locationBtn = document.getElementById('locationBtn');
+const recentCities = document.getElementById('recentCitiesList');
+const cityName = document.getElementById('cityName');
+const error = document.getElementById('errorDetails');
+const currentWeather = document.getElementById('currentWeatherDetails');
+const forecastDetails = document.getElementById('forecast');
+const fiveDayData = document.getElementById('fiveDays')
 
-searchBtn.addEventListener('click', () => searchCity(cityInput.value));
-locBtn.addEventListener('click', getCurrentLocation);
-recentCitiesEl.addEventListener('change', () => searchCity(recentCitiesEl.value));
+//Adding event listeners on buttons
+searchCityBtn.addEventListener('click', () => searchCity(cityName.value));
+locationBtn.addEventListener('click', getCurrentLocation);
+recentCities.addEventListener('change', () => searchCity(recentCities.value));
 
 
-
+//function definition to search city
 function searchCity(city) {
-    if (!city) return showError('Please enter a city name');
-    hideError();
-    fetchWeather(city);
-    updateRecentCities(city);
+  if (!city) return showError('Please enter a city name');          //error handling if user tries to search with no input
+  hideError();                                                      //hide error after displaying
+  fetchWeather(city);                                               //calling fetchWeather function with current city input
+  updateRecentCities(city);                                         //updating recent cities list to add new city
 }
 
-function updateRecentCities(city) {
-    let cities = JSON.parse(localStorage.getItem('recentCities')) || [];
-    if (!cities.includes(city)) {
-      cities.unshift(city);
-      if (cities.length > 5) cities.pop();
-      localStorage.setItem('recentCities', JSON.stringify(cities));
-    }
-    renderRecentCities();
+//function to store and update recent cities
+function updateRecentCities(city) {                                             //taking input of searched city
+  let cities = JSON.parse(localStorage.getItem('recentCitiesList')) || [];    //retreiving recent cities from local storage
+  if (!cities.includes(city)) {
+    cities.unshift(city);                                                     //adding new city at start if not already present in list
+    if (cities.length > 5) cities.pop();                                      //only 5 recently searched cities should display in dropdown
+    localStorage.setItem('recentCitiesList', JSON.stringify(cities));         //update and store new list in local storage
+  }
+  renderRecentCities();
 }
 
+//fetch weather details from API with city name
 function fetchWeather(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
-      .then(res => {
-        console.log(res);
-        if (!res.ok) throw new Error('City not found');
-        return res.json();
-      })
-      .then(data => displayWeather(data))
-      .catch(err => showError(err.message));
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)    //fetching weather details for required city using API
+    //retreiving response from API, and sending it to display, with error handling
+    .then(res => {
+      if (!res.ok) throw new Error('City not found');               //error handling if no data received for searched city
+      return res.json();                                            //returning received data if no error
+    })
+    .then(data => displayWeather(data))                             //sending data to be displayed
+    .catch(err => showError(err.message));                          //show error in console if data not received
 }
 
+//fetch weather details from API with location coordinates
 function fetchWeatherByCoords(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
-      .then(res => {
-        if (!res.ok) throw new Error('Weather data not available');
-        return res.json();
-      })
-      .then(data => displayWeather(data))
-      .catch(err => showError(err.message));
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+    .then(res => {
+      if (!res.ok) throw new Error('Weather data not available for given coordinates');
+      return res.json();
+    })
+    .then(data => displayWeather(data))
+    .catch(err => showError(err.message));
   }
 
-
+//get current location to be passed to fetchWeatherByCoords
 function getCurrentLocation() {
-    hideError();
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        fetchWeatherByCoords(latitude, longitude);
-      }, () => showError('Unable to access location.'));
-    } else {
-      showError('Geolocation not supported.');
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      fetchWeatherByCoords(latitude, longitude);                     //passing coordinate details to fetchWeatherByCoords function to fetch weather from API using coords
+    //error handling
+    }, () => showError('Unable to access location.'));
+  } else {
+    showError('Geolocation not supported.');
+  }
 }
 
+//displaying recent cities from list in dropdown
 function renderRecentCities() {
-    const cities = JSON.parse(localStorage.getItem('recentCities')) || [];
+    const cities = JSON.parse(localStorage.getItem('recentCitiesList')) || [];        //retreiving list of recent cities from local storage
     if (cities.length > 0) {
-      recentCitiesEl.classList.remove('hidden');
-      recentCitiesEl.innerHTML = '<option selected disabled>Recent Cities</option>' + cities.map(city => `<option value="${city}">${city}</option>`).join('');
+      recentCities.classList.remove('hidden');
+      recentCities.innerHTML = '<option selected disabled>Recent Cities</option>' + cities.map(city => `<option value="${city}">${city}</option>`).join('');
     } else {
-      recentCitiesEl.classList.add('hidden');
+      recentCities.classList.add('hidden');
     }
 }
   
@@ -99,12 +105,12 @@ function renderRecentCities() {
 
  
 function showError(msg) {
-    errorEl.textContent = msg;
-    errorEl.classList.remove('hidden');
+    error.textContent = msg;
+    error.classList.remove('hidden');
 }
   
 function hideError() {
-    errorEl.classList.add('hidden');
+    error.classList.add('hidden');
 }
 
 
@@ -112,7 +118,7 @@ function hideError() {
     const cityName = data.city.name;
     const current = data.list[0];
   
-    currentWeatherEl.innerHTML = `
+    currentWeather.innerHTML = `
       <div class="text-center">
         <h2 class="text-xl italic">Currently in ${cityName}</h2>
         <img src="https://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png" class="mx-auto" />
@@ -121,12 +127,12 @@ function hideError() {
       </div>
     `;
 
-    fiveDays.innerHTML = `<h3 class="text-xl font-bold text-center p-4" id="fiveDay">5-Days Forecast for ${cityName}</h3>`
+    fiveDayData.innerHTML = `<h3 class="text-xl font-bold text-center p-4" id="fiveDays">5-Days Forecast for ${cityName}</h3>`
   
-    forecastEl.innerHTML = '';
+    forecastDetails.innerHTML = '';
     for (let i = 0; i < data.list.length; i += 8) {
       const forecast = data.list[i];
-      forecastEl.innerHTML += `
+      forecastDetails.innerHTML += `
         <div class="w-full flex flex-col items-center justify-center gap-1 border-black m-1 text-slate-900 rounded-lg py-2 px-4 shadow-lg shadow-black text-center min-w-[250px] flex-shrink-2 snap-center lg:min-w-0 md:flex-shrink-2 lg:flex-shrink-5">
           <h3 class="font-semibold">${new Date(forecast.dt_txt).toDateString()}</h3>
           <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" class="mx-auto" />
